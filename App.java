3211ba1273d7a4bb29aae13b858f23d850cc2fca
Kiart Tantasi <kiarttantasi@Kiarts-MacBook-Pro.local>
2023-10-11 18:03:13 +0700

    create API using com.sun.net.httpserver

---
+++ App.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+// TODO: using Servlets container (Apache Tomcat)
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        final int port = 8080;
+        final HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/healthz", new HealthzHandler());
+        server.setExecutor(null); // Use the default executor
+        server.start();
+        System.out.println("Server is running on port " + port);
+    }
+}
+
+class HealthzHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        try {
+            final String response = "{\"message\": \"Some message sent !\"}";
+            // needs to set heder before send header
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.length());
+            final OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (Exception ex) {
+            System.out.println("HealthzHandler Error: " + ex.getMessage());
+            throw ex;
+        }
+    }
+}
